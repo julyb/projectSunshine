edf072c6ce16ccba2d631814c7990bd553f76977
Daniela Bulgaru <danielablg@gmail.com>
2017-04-30 04:02:19 +0100

    Merged changes

---
+++ projectSunshine/LocationEnergyDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LocationEnergyDetailViewController.swift
+//  projectSunshine
+//
+//  Created by Daniela Bulgaru on 30/04/2017.
+//  Copyright © 2017 Iulia Baltoi. All rights reserved.
+//
+
+import UIKit
+
+final class LocationEnergyDetailViewController: UIViewController {
+  
+  let solarPanelInformation = [
+    "solar-panel-1" : "Monocrystalline \nMonocrystalline solar panels use thin wafers from single silicon crystals, which have been grown for this exact purpose, as their semiconductor. The fact that they do not mix the silicon gives them a uniform look. It also makes them the most efficient style of solar panel. As a buyer this means that monocrystalline solar panels may be the most expensive, but they will also produce the most electricity.",
+    
+    "solar-panel-2" : "Polycrystalline \nPolycrystalline panels use off cuts of different silicon crystals, melted down into a mould, to form their semiconductor. This disjointed nature makes them slightly less efficient than monocrystalline panels. However, the difference in the efficiency of mono/polycrystalline panels used to be more noticeable than it is now. Developments in crystalline technology have lead to monocrystalline and polycrystalline solar systems having a very similar price and output. One of the only real differences left is their look. Polycrystalline panels seem to have a bluer look due to the various crystal pieces reflecting the light.",
+    
+    "solar-panel-3" : "Thin film / amorphous silicon \nThin film solar panels are a newer technology than crystalline panels. Thin film panels use an incredibly thin layer of amorphous silicon as their semiconductor. This method makes the panels overall thinner and more aesthetically pleasing. Thin film panels can be more flexible and more easily mounted onto a variety of surfaces. They can also work more efficiently in low light and cloudy conditions. Thin film panels are a cheaper than crystalline panels to make and install.\nHowever, thin film panels are not yet as efficient as crystalline panels. Crystalline panels are still up to 4 times more efficient than thin film, meaning that you will get a lot less electricity per square meter from thin film panels.",
+    
+    "solar-panel-4" : "Hybrid \nThere is a new type of hybrid solar panel making it’s way slowly onto the mainstream market. Hybrid panels are essentially PV panels with heat exchangers underneath which syphon off any excess heat created by the panels. Solar panels do not need heat to work, they only need light. However, panels can become hot over the course of the day and too much heat can actually reduce their efficiency. With this in mind, engineers have created a way to use that excess heat to create hot water for the household to use. \nThis technology could be a great addition to any home in the future. The solar panels will produce more electricity because they will not overheat and the house will benefit from free hot water too. However, the technology is still very new, this means that it is still very expensive! Hybrid panels have not yet been tested over the long term to determine their return on investment. \nCurrently the government runs two seperate incentives for those who install solar panels, the Feed-in Tariff for solar PV and the Renewable Heat incentive for solar thermal. With hybrid panels you will pay a lot more for installation but you will still only receive the Feed-in payments, the same as you would with a normal PV system.",
+    
+    "solar-panel-5" : "",
+    "solar-panel-6" : "",
+    "solar-panel-7" : "",
+    "solar-panel-8" : "",
+    "solar-panel-9" : "",
+  ]
+  
+}
